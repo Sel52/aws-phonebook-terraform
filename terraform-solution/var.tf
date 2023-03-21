df47998867e5b163cd6de38871d77b2ff3c2e9d5
Sel52 <ank.et.el.061@gmail.com>
2023-03-21 13:07:26 +0300

    terraform-added

---
+++ terraform-solution/var.tf
@@ -0,0 +1,16 @@
+variable "git-name" {
+  default = "Sel52"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxx" #enter your token
+}
+
+variable "key-name" {
+  default = "xxxxxxx" #enter your key pem
+}
+
+variable "hosted-zone" {
+  default = "xxxxxxxxxxx"#enter your  host zone
+}
+
